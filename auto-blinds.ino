2ffaa3abf916c0fbfb494b1c516dc4d1f67b4623
kimbsy <lordkimber@gmail.com>
2019-07-08 19:13:07 +0100

    updating to use nema 17 motor and drv8825 driver, added first pass fritzing diagram

--- auto-blinds.ino
+++ auto-blinds.ino
@@ -1,110 +1,40 @@
-#define OUT1 2
-#define OUT2 3
-#define OUT3 4
-#define OUT4 5
+#define STEP_PIN 3
+#define DIR_PIN 4
 #define IN1 6
 #define IN2 7
 
-int steps = 0;
-int direction = 1;
+int direction = 0;
 
 void setup() {
+  pinMode(STEP_PIN, OUTPUT); 
+  pinMode(DIR_PIN, OUTPUT);
   Serial.begin(115200);
-  pinMode(OUT1, OUTPUT);
-  pinMode(OUT2, OUTPUT);
-  pinMode(OUT3, OUTPUT);
-  pinMode(OUT4, OUTPUT);
-  pinMode(IN1, INPUT);
-  pinMode(IN2, INPUT);
-  delay(1000);
 }
 
 void loop() {
   getInput();
-  doSteps(1);
+  if (direction != -1) {
+    doSteps(1);
+  }
 }
 
 void getInput() {
-  Serial.println(digitalRead(IN1));
-  Serial.println(digitalRead(IN2));
   if (digitalRead(IN1) == HIGH) {
-    direction = 1;
+    direction = HIGH;
+    digitalWrite(DIR_PIN, direction);
   } else if (digitalRead(IN2) == HIGH) {
-    direction = -1;
+    direction = LOW;
+    digitalWrite(DIR_PIN, direction);
   } else {
-    direction = 0;
+    direction = -1;
   }
-}
-
-void doRotations(int rotationCount) {
-  doSteps(4096 * rotationCount);
 }
 
 void doSteps(int stepCount) {
-  for (int i = 0; i < stepCount; i++) {
-    switch(steps) {
-      case 0:
-        digitalWrite(OUT1, LOW);
-        digitalWrite(OUT2, LOW);
-        digitalWrite(OUT3, LOW);
-        digitalWrite(OUT4, HIGH);
-      break;
-      case 1:
-        digitalWrite(OUT1, LOW);
-        digitalWrite(OUT2, LOW);
-        digitalWrite(OUT3, HIGH);
-        digitalWrite(OUT4, HIGH);
-      break;
-      case 2:
-        digitalWrite(OUT1, LOW);
-        digitalWrite(OUT2, LOW);
-        digitalWrite(OUT3, HIGH);
-        digitalWrite(OUT4, LOW);
-      break;
-      case 3:
-        digitalWrite(OUT1, LOW);
-        digitalWrite(OUT2, HIGH);
-        digitalWrite(OUT3, HIGH);
-        digitalWrite(OUT4, LOW);
-      break;
-      case 4:
-        digitalWrite(OUT1, LOW);
-        digitalWrite(OUT2, HIGH);
-        digitalWrite(OUT3, LOW);
-        digitalWrite(OUT4, LOW);
-      break;
-      case 5:
-        digitalWrite(OUT1, HIGH);
-        digitalWrite(OUT2, HIGH);
-        digitalWrite(OUT3, LOW);
-        digitalWrite(OUT4, LOW);
-      break;
-        case 6:
-        digitalWrite(OUT1, HIGH);
-        digitalWrite(OUT2, LOW);
-        digitalWrite(OUT3, LOW);
-        digitalWrite(OUT4, LOW);
-      break;
-      case 7:
-        digitalWrite(OUT1, HIGH);
-        digitalWrite(OUT2, LOW);
-        digitalWrite(OUT3, LOW);
-        digitalWrite(OUT4, HIGH);
-      break;
-      default:
-        digitalWrite(OUT1, LOW);
-        digitalWrite(OUT2, LOW);
-        digitalWrite(OUT3, LOW);
-        digitalWrite(OUT4, LOW);
-      break;
-    }
-    incByDirection();
-    delay(1);
+  for(int i = 0; i < stepCount; i++) {
+    digitalWrite(STEP_PIN, HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(STEP_PIN, LOW); 
+    delayMicroseconds(500); 
   }
-}
-
-void incByDirection() {
-  steps += direction;
-  if (steps > 7) { steps = 0; }
-  if (steps < 0) { steps = 7; }
 }
